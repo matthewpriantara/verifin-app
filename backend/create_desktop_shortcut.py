9df2bfd33bda4db8d24687005d1af80b64422dda
Hafidz Prasetya <hafidzrizqullahprasetya@mail.ugm.ac.id>
2026-08-18 00:33:06 +0700

    fix(desktop): support OneDrive Desktop path for shortcut creation

--- backend/create_desktop_shortcut.py
+++ backend/create_desktop_shortcut.py
@@ -1,12 +1,18 @@
 import os
 
-desktop = os.path.join(os.environ["USERPROFILE"], "Desktop")
-shortcut_path = os.path.join(desktop, "Start Verifin Backend.lnk")
 target = r"C:\Users\fizzu\Documents\Github\gemastik19\verifin-app\backend\START_BACKEND.bat"
 work_dir = r"C:\Users\fizzu\Documents\Github\gemastik19\verifin-app\backend"
 icon = r"C:\Users\fizzu\Documents\Github\gemastik19\verifin-app\frontend\public\favicon.ico"
 
-vbs_content = f'''Set oWS = WScript.CreateObject("WScript.Shell")
+desktops = [
+    r"C:\Users\fizzu\OneDrive\Desktop",
+    os.path.join(os.environ["USERPROFILE"], "Desktop"),
+]
+
+for d in set(desktops):
+    if os.path.exists(d):
+        shortcut_path = os.path.join(d, "Start Verifin Backend.lnk")
+        vbs_content = f'''Set oWS = WScript.CreateObject("WScript.Shell")
 sLinkFile = "{shortcut_path}"
 Set oLink = oWS.CreateShortcut(sLinkFile)
 oLink.TargetPath = "{target}"
@@ -15,17 +21,14 @@ oLink.IconLocation = "{icon}"
 oLink.Description = "Start Verifin FastAPI Backend Server"
 oLink.Save
 '''
+        vbs_path = os.path.join(work_dir, "_make_shortcut.vbs")
+        with open(vbs_path, "w") as f:
+            f.write(vbs_content)
 
-vbs_path = os.path.join(work_dir, "_make_shortcut.vbs")
-with open(vbs_path, "w") as f:
-    f.write(vbs_content)
+        os.system(f'cscript //nologo "{vbs_path}"')
 
-os.system(f'cscript //nologo "{vbs_path}"')
+        if os.path.exists(vbs_path):
+            os.remove(vbs_path)
 
-if os.path.exists(vbs_path):
-    os.remove(vbs_path)
-
-if os.path.exists(shortcut_path):
-    print("SUCCESS: Shortcut created on Desktop ->", shortcut_path)
-else:
-    print("FAILED to create shortcut")
+        if os.path.exists(shortcut_path):
+            print("SUCCESS: Created shortcut at ->", shortcut_path)
